056e72a21caecd3dbb2e668af57a3fc844ea0858
ThuongDT <thuongdt@ezg.vn>
2025-05-04 10:35:04 +0700

    update prompt to chat bot

--- open_ai/ChatGPT.py
+++ open_ai/ChatGPT.py
@@ -1,25 +1,34 @@
-import openai
+import os
 import json
 import requests
-import service.Service as service
+import service.HandlingJson as JsonService
 
 
-def create_content_request(path_file):
-    data_from_file_json = service.read_json(path_file)
-
+def create_first_request():
     prompt = f"""
-    Dưới đây là một chuỗi JSON chứa các đoạn hội thoại trao đổi công việc giữa các đồng nghiệp trong một ngày.
-    Hãy phân tích và viết lại **báo cáo công việc** của ngày hôm đó, dưới dạng **các gạch đầu dòng**, ngắn gọn, rõ ràng và đúng trọng tâm.
-    JSON tin nhắn:
-    {json.dumps(data_from_file_json, indent=2)}
+    Dưới đây gửi cho bạn nhiều chuỗi JSON chứa các đoạn hội thoại trao đổi công việc giữa các đồng nghiệp trong một ngày.
+    Tôi cần bạn đọc và hiểu nội dung để thực hiện các yêu cầu của tôi sau này.
+    Sau đây tôi sẽ gửi bạn nội dung bạn chỉ cần nhận thông tin và phân tích ngầm thôi không cần gửi lại phần phân tích ngay.
     """
 
     return prompt
 
-def create_summary_command(path_file):
-    data_from_file_json = service.read_json(path_file)
+def create_mid_command(path_file):
+    data_from_file_json = JsonService.read_json(path_file)
 
-    prompt = f"Bạn hãy đọc dữ liệu tôi cung cấp và hãy tìm hiểu xem đó là gì. Dữ liệu như sau: {json.dumps(data_from_file_json, indent=2)}"
+    file_name = os.path.basename(path_file)
+    file_stem = os.path.splitext(file_name)[0]
+
+    prompt = f"""Đây là dữ liệu của file {file_stem} như sau: 
+    {json.dumps(data_from_file_json, indent=2)}"""
+
+    return prompt
+
+def create_final_command():
+    prompt = f"""Dựa vào những gì tôi vừa cung cấp bạn hãy phân tích và viết lại **báo cáo công việc** của ngày hôm đó, 
+    dưới dạng **các gạch đầu dòng**, ngắn gọn, rõ ràng và đúng trọng tâm. 
+    Đồng thời có bạn có thể đưa ra dự đoán về tiến độ công việc có thể hoàn thành trong tuần hay không 
+    **Không bắt buộc, nếu đủ dữ liệu để dự đoán**"""
 
     return prompt
 
@@ -47,7 +56,7 @@ def request_gpt(prompt):
     )
 
     response_json = request_to_gpt.json()
-    service.write_file_json("output/output_gpt.json", response_json)
+    JsonService.write_file_json("output/output_gpt.json", response_json)
 
     response = response_json["choices"][0]["message"]["content"]
     return response
